ad94267327b140c817a047daa901100c659b8974
dstepanenko <dstepanenko@aq.ru>
2025-05-06 07:40:29 +0300

    API драйвера I2C интерфейса микроконтроллера

---
+++ Core/Src/i2c_hw.c
@@ -0,0 +1,35 @@
+#include "i2c_hw.h"
+
+i2chw_error_t I2CHW_Init(const i2chw_bus_t bus_num)
+{
+
+}
+
+i2chw_error_t I2CHW_Configure(const i2chw_bus_t bus_num,
+			      const i2chw_cfg_t *p_config)
+{
+
+}
+
+i2chw_error_t I2CHW_WriteSync(const i2chw_dev_t *p_dev,
+			      const uint8_t *p_tx_data,
+			      const uint16_t tx_data_len)
+{
+
+}
+
+i2chw_error_t I2CHW_WriteReadSync(const i2chw_dev_t *p_dev,
+				  const uint8_t *p_tx_data,
+				  const uint16_t tx_data_len,
+				  uint8_t *p_rx_data,
+				  const uint16_t rx_data_len)
+{
+
+}
+
+i2chw_error_t I2CHW_ReadSync(const i2chw_dev_t *p_dev,
+			     uint8_t *p_rx_data,
+			     const uint16_t rx_data_len)
+{
+
+}
